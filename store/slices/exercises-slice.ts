5385b142e6161aff94207fb454595136d7a27d69
Augustus Murphy <gusbmurphy@gmail.com>
2021-01-14 12:01:06 -0500

    Implement whitespace removal and dupe incrementing

--- store/slices/exercises-slice.ts
+++ store/slices/exercises-slice.ts
@@ -7,7 +7,7 @@ type ExercisesSliceState = {
   items: Exercise[];
 };
 
-const initialState: ExercisesSliceState = {
+export const initialState: ExercisesSliceState = {
   items: [],
 };
 
@@ -16,8 +16,35 @@ const exercisesSlice = createSlice({
   initialState,
   reducers: {
     addExercise(state, action: PayloadAction<{title: string}>) {
+      let {title} = action.payload;
+
+      // Trim any excess whitespaces at end of title...
+      title = title.replace(/\s+$/g, '');
+
+      /* Check to see if there's another exercise with this name, if so, add a number to the end.
+      We'll use a regular expression to ensure that we are finding an item with just the name (as
+      opposed to simply including it in a larger string), and possibly a number that we've appended
+      to it. */
+      let titleRegExp = new RegExp(`^${title}(?:\s\d+)?$`, 'gi');
+      let indexOfExistingTitle = state.items.findIndex((item) =>
+        titleRegExp.test(item.title),
+      );
+
+      if (indexOfExistingTitle !== -1) {
+        let appendedNum = 1;
+        let numRegExp = new RegExp(`^${title}\s{appendedNum}$`);
+
+        while (
+          state.items.findIndex((item) => numRegExp.test(item.title)) != -1
+        ) {
+          appendedNum++;
+        }
+
+        title += ' ' + appendedNum;
+      }
+
       state.items.push({
-        title: action.payload.title,
+        title: title,
         id: uuid(),
         tagIds: [],
         parameters: [],
