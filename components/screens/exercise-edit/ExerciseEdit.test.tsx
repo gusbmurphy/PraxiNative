620f0a13d6cec6372b91aa5b847e68a737965cc2
Augustus Murphy <gusbmurphy@gmail.com>
2021-02-08 15:45:45 -0600

    Add ExerciseEdit tests

---
+++ components/screens/exercise-edit/ExerciseEdit.test.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {ExerciseListScreen} from './ExerciseList';
+import {store} from '../../store';
+import {Provider as StoreProvider} from 'react-redux';
+import {createStackNavigator} from '@react-navigation/stack';
+import {RootStackParamList} from '../../App';
+import {fireEvent, render, waitFor} from '@testing-library/react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {RootStackParamList} from '../../../App';
+import {ExerciseEditScreen} from '.';
+
+// Silence the warning https://github.com/facebook/react-native/issues/11094#issuecomment-263240420
+jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
+
+describe('Exercise Edit Screen', () => {
+  const RootStack = createStackNavigator<RootStackParamList>();
+
+  const screen = (
+    <StoreProvider store={store}>
+      <PaperProvider>
+        <NavigationContainer>
+          <RootStack.Navigator>
+            <RootStack.Screen
+              name="ExerciseEdit"
+              component={ExerciseEditScreen}
+            />
+          </RootStack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </StoreProvider>
+  );
+
+  const {getAllByA11yLabel} = render(screen);
+
+  test.todo('has a text input that corresponds to the name');
+  test.todo(
+    'has a section titled "Tags" that shows the current tags for the exercise, and has a button that brings up a modal to add a tag',
+  );
+  test.todo(
+    'has section titled "Parameters" that shows the current parameters, and has a button that brings up a modal to add new parameters',
+  );
+});
